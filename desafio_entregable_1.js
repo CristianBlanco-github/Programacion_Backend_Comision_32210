571e2ce9dce6042c385e0dab01f66d542c984258
cristianblanco <blancochoquecristian123@gmail.com>
2022-11-22 22:41:48 -0300

    Entrega de desafio 1

--- desafio_entregable_1.js
+++ desafio_entregable_1.js
@@ -1,10 +1,8 @@
 class ProductManager{
-    #precioBaseDeGanancia
     constructor(){
         this.products=[]
-        this.precioBaseDeGanancia=0.15
     }
-    getEvents=()=>{return this.products}
+    getProducts=()=>{return this.products}
     //Consigue el siguiente ID
     getNextID=()=>{
         const count=this.products.length
@@ -17,13 +15,7 @@ class ProductManager{
         
     }
     getcode=()=>{
-        const count=this.products.length
-        if(count==0) return 1
-        const lastEvent=this.products[count-1]
-        const lastID=lastEvent.id
-        const nextID=lastID+1
-        // const nextID = (amount > 0) ? this.products[amount - 1].id + 1 : 1;
-            return nextID
+        return parseInt(Math.random() * 1000) 
     }
 
     addProduct=(title, description, price,thumbnail, stock)=>{
@@ -33,47 +25,39 @@ class ProductManager{
             id,
             title,
             description,
-            priceBase:price,
-            price:price*(1+ this.#precioBaseDeGanancia),
+            price,
             thumbnail:thumbnail ??'Sin imagen',
             code,
             stock :stock ??50
         }
-        this.products.push(event)
+
+        // const sameId = (element) => element === event.code;
+        // if (!this.products.some(sameId)) {
+            this.products.push(event)
+        // } else {
+        //     console.log("An id is duplicated")
+        // }
         
     }
     addcode=(eventID,code)=>{
         const event=this.products.find(event=>event.id==eventID)
         return event.some(e=>e.codigo === code); 
     }
-    // addParticipant=(eventID, userID)=>{
-    //     const event=this.products.find(event=>event.id==eventID)
-    //     if(event==undefined)return-1
-    //     if(!event.code.includes(userID)){
-    //         event.code.push(userID)
-    //         return 1
-    //     }
-    //     return -1
-    // }
-
-
-
-    // ponerEventoEnGira = (eventID, descriptionNew) => {
-    //     const event = this.products.find(event => event.id == eventID)
-    //     const { title, priceBase, stock,thumbnail  } = event
-    //     this.addProduct(title, descriptionNew, priceBase, thumbnail, stock )
-    // }
+    getProductById=(eventID)=>{
+        const event=this.products.filter(product=>product.id==eventID)
+        console.log(event)
+        if(event.length===0){
+            return `no exite`
+        }else{
+            return `existe`
+        }
+    }
 }
-const ticketManager=new ProductManager()
-console.log(ticketManager.addProduct('remera','corto rosa',120,0,30,23))
-console.log(ticketManager.addProduct('gorra','azul x',100,0,30,23))
-console.log(ticketManager.addProduct('pera',100,0,0))
 
-// console.log(ticketManager.addcode(1,333))
-// console.log(ticketManager.addcode(3,444))
-// console.log(ticketManager.addcode(1,333))
-// ticketManager.addParticipant(2,'German')
-// ticketManager.addParticipant(2,'Facundo')
-// ticketManager.ponerEventoEnGira(1,'Buenos Aires',null)
+const productos=new ProductManager()
+productos.addProduct('remera','corto rosa',120,null,10)
+productos.addProduct('gorra','azul x',100,null,30)
+productos.addProduct('pantalon','negro',0,null,10)
 
-console.log(ticketManager.products)
+console.log(productos.products)
+console.log(productos.getProductById(5))
